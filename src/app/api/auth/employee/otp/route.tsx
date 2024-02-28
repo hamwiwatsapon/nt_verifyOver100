691c0a4c8e2a548615c244b25aa3ac77d2eaed15
Wiwatsapon Lertworasirikul <hamlert33@gmail.com>
2024-02-28 11:18:35 +0700

    Update route.tsx

--- src/app/api/auth/employee/otp/route.tsx
+++ src/app/api/auth/employee/otp/route.tsx
@@ -32,7 +32,7 @@ export async function POST(req:Request) {
   }
 
   try {
-    const url = `https://customerapi.cattelecom.com/app/customerapi/v200/search/customer?subscriber_number=${msisdn_cut}&is_active=true`
+    const url = `https://customerapi.xxxxx.com/app/customerapi/v200/search/customer?subscriber_number=${msisdn_cut}&is_active=true`
     const res = await axios.get(url, {headers: {apikey: process.env.PROD_API_KEY}});
     const resData = res.data;
     const idCardNumber = resData.customer_search[0].id_card_number;
@@ -40,7 +40,7 @@ export async function POST(req:Request) {
     const time = new Date();
     const formattedTime = time.toLocaleString().replace(/T/, ' ').replace(/\..+/, '');
 
-    const url_2 = `https://customerapi.cattelecom.com/app/customerapi/v200/search/customer/fetch?id_card_number=${idCardNumber}&is_active=true`
+    const url_2 = `https://customerapi.xxxxx.com/app/customerapi/v200/search/customer/fetch?id_card_number=${idCardNumber}&is_active=true`
     const res_2 = await axios.get(url_2, {headers: {apikey: process.env.PROD_API_KEY}});
     const res2Data = res_2.data
     
@@ -52,25 +52,25 @@ export async function POST(req:Request) {
         <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
             <soap:Body>
             <smssoap xmlns="http://microsoft.com/webservices/">
-                <Username>rtcopr</Username>
-                <Password>myRTC@brk</Password>
-                <Sender>my</Sender>
+                <Username>xxxxxx</Username>
+                <Password>xxxxxxxx</Password>
+                <Sender>xxxxx</Sender>
                 <Destination>${msisdn}</Destination>
                 <Message>${message}</Message>
                 <Lang>T</Lang>
             </smssoap>
             </soap:Body>
         </soap:Envelope>`
-        // const res = await axios.post(url, body,{headers: {"Content-Type": "text/xml; charset: utf-8"}})
-        // if (res.status === 200) {
+          const res = await axios.post(url, body,{headers: {"Content-Type": "text/xml; charset: utf-8"}})
+          if (res.status === 200) {
           const log = `${formattedTime}|${EmployeeId}|EMPLOYEE|${msisdn}|SUCCESS\n`
           fs.appendFile('./log/login.log', log, () => {});
           console.log(OTP)
           return NextResponse.json({ status: 200, otpData: OTP, customerData: res2Data});
-        // } 
-        // const log = `${formattedTime}|${EmployeeId}|EMPLOYEE|${msisdn}|INVALID\n`
-        // fs.appendFile('./log/login.log', log, () => {});
-        // return NextResponse.json({ status: 401 });
+        } 
+        const log = `${formattedTime}|${EmployeeId}|EMPLOYEE|${msisdn}|INVALID\n`
+        fs.appendFile('./log/login.log', log, () => {});
+        return NextResponse.json({ status: 401 });
       }
   } catch (err) {
     const time = new Date();
