900a6d3a6acb50212965a32b82828b1c19e7b4fd
Wiwatsapon Lertworasirikul <hamlert33@gmail.com>
2024-02-28 11:17:17 +0700

    Update route.tsx

--- src/app/api/auth/customer/route.tsx
+++ src/app/api/auth/customer/route.tsx
@@ -48,8 +48,8 @@ export async function POST(req:Request) {
       <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
           <soap:Body>
           <smssoap xmlns="http://microsoft.com/webservices/">
-              <Username>rtcopr</Username>
-              <Password>myRTC@brk</Password>
+              <Username>xxxxx</Username>
+              <Password>xxxxxxxxxxx</Password>
               <Sender>my</Sender>
               <Destination>${msisdn}</Destination>
               <Message>${message}</Message>
